ae3f7d3d7fd44f52f901cc0e70f3fd099fc1c232
CJSmekens <42803564+CJSmekens@users.noreply.github.com>
2021-06-07 15:41:08 +0200

    Exercise on documenting code

--- exercise_documentation.py
+++ exercise_documentation.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 def x(a, print_columns=False):
 """ 
-Print column headers of excel file
+It parses an excel file and returns a list containing column headers.
 
-Args:
-  a (pandas.String): path to excel file
-  print_columns (boolian): True/False
-  
-Returns:
-  list containing the column headers
+args:
+    a (String): String containing the path to an excel file.
+    print_columns (Bool): If true, the method prints the header of each column of file a. Default: False.
+    
+returns:
+    column_headers (list): A list containing all column headers in an excel file.
 """
    b = pd.read_excel(a)
    column_headers = list(b.columns.values)
