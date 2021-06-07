c6efe4054dcf250bc4029ec7459c19a325ee082f
CJSmekens <42803564+CJSmekens@users.noreply.github.com>
2021-06-07 15:44:25 +0200

    Update exercise_documentation.py

--- exercise_documentation.py
+++ exercise_documentation.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-def x(a, print_columns=False):
+def get_spreadsheet_columns(file_loc, print_columns=False):
 """ 
 It parses an excel file and returns a list containing column headers.
 
 args:
-    a (String): String containing the path to an excel file.
-    print_columns (Bool): If true, the method prints the header of each column of file a. Default: False.
+    file_loc (String): String containing the path to an excel file.
+    print_columns (Bool, optional): If true, the method prints the header of each column of file a. Default: False.
     
 returns:
     column_headers (list): A list containing all column headers in an excel file.
 """
-   b = pd.read_excel(a)
-   column_headers = list(b.columns.values)
+   speadsheet_info = pd.read_excel(file_loc)
+   column_headers = list(speadsheet_info.columns.values)
    if print_columns:
        print("\n".join(column_headers))
    return column_headers
